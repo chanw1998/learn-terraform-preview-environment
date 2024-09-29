eafbcae154baf39efdd8bf815ebd8d8dc229f74b
Winston Chan <chanw.1998@gmail.com>
2024-09-28 23:39:32 -0400

    Update Vercel project name and TFC organization name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "AsperisSolutionsInc"
     workspaces {
       tags = ["hashicupsBackend"]
     }
